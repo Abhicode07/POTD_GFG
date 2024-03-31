f1c61112d595672c2fb0f7f4260fad938d48e567
Abhishek Sanjay Bhavsar <91628964+Abhicode07@users.noreply.github.com>
2024-03-31 21:27:25 +0530

    Create Closest Neighbour in BST.java

---
+++ Closest Neighbour in BST.java
@@ -0,0 +1,15 @@
+class Solution {
+    public static int findMaxForN(Node root, int n) {
+        Node curr = root;
+        int res = -1;
+        while(curr != null){
+            if(curr.key <= n){
+                res = curr.key;
+                curr = curr.right;
+            }else{
+                curr = curr.left;
+            }
+        }
+        return res;
+    }
+}
