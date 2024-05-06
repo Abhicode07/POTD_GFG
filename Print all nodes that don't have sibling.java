91c64f4d624efbfbc5dd6a8a15178e7d2850cca1
Abhishek Sanjay Bhavsar <91628964+Abhicode07@users.noreply.github.com>
2024-05-06 23:29:21 +0530

    Create Print all nodes that don't have sibling.java

---
+++ Print all nodes that don't have sibling.java
@@ -0,0 +1,18 @@
+ class Tree
+{
+    ArrayList<Integer> noSibling(Node node)    
+    {
+        ArrayList<Integer> ans = new ArrayList<>();
+        noSibling(node, ans);
+        Collections.sort(ans);
+        if(ans.size() == 0) ans.add(-1);
+        return ans;
+    }
+    void noSibling(Node node, ArrayList<Integer> ans){
+        if(node == null || (node.left == null && node.right == null)) return;
+        if(node.left == null) ans.add(node.right.data);
+        if(node.right == null) ans.add(node.left.data);
+        noSibling(node.left, ans);
+        noSibling(node.right, ans);
+    }
+}
